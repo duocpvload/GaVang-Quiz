f20cd5b473b252f1e1f209a8eea8a2bf0035f078
duocpvload <duocpv.load@gmail.com>
2026-06-21 10:01:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -167,8 +167,8 @@ setTimeout(()=>{
         ⏱️ Thời gian:
         ${minutes} phút ${seconds} giây
         </p>
-        <button onclick="showRanking(${currentSubject})">
-            🏆 BXH ${currentSubject}
+        <button onclick="showRanking('${currentSubject}')">
+            🏆 Bảng xếp hạng
         </button>
         <button onclick="location.reload()">
             Chơi lại
